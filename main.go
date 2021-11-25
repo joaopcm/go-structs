7c670054da06f6534843e5c3ca11e0ae15a8029c
João Melo <jopcmelo@gmail.com>
2021-11-25 08:30:52 -0300

    fix: work with pointer operations

--- main.go
+++ main.go
@@ -29,13 +29,13 @@ func main() {
 		},
 	}
 
-	jim.updateName("Josh")
-
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
